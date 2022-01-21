58ed375ad64b50e0c21a0b004db6f5ec051da78b
Will Greenberg <greenberg.will@gmail.com>
2022-01-21 15:43:03 -0800

    add second form to ui logic, add commas and spaces

--- js/scripts.js
+++ js/scripts.js
@@ -13,28 +13,46 @@ function beepBoop(input){
     for (i = 0; i <= input; i += 1) {
       if (i === 0) {
         outputArray.push("0")
+        if (i != input){
+          outputArray.push(", ")
+        }
       } else if (i > 9) {
         let indexString = i.toString();
         let indexArray = indexString.split("");
         if (indexArray.includes("3")){
-          outputArray.push(" Won't you be my neighbor?");
+          outputArray.push("Won't you be my neighbor?");
+          if (i != input){
+            outputArray.push(", ")
+          }
         } else if (indexArray.includes("2")){
-          outputArray.push(" Boop!");
+          outputArray.push("Boop!");
+          if (i != input){
+            outputArray.push(", ")
+          }
         } else if (indexArray.includes("1")){
-          outputArray.push(" Beep!");
+          outputArray.push("Beep!");
+          if (i != input){
+            outputArray.push(", ")
+          }
         } else {
         let joinedIndex = indexArray.join("");
         let joinedNumber = parseInt(joinedIndex);
         outputArray.push(joinedNumber);
+        if (i != input){
+          outputArray.push(", ")
         }
+      }
       } else if (i === 1) {
-        outputArray.push(" Beep!")
+        outputArray.push(" Beep!, ")
       } else if (i === 2) {
-        outputArray.push(" Boop!")
+        outputArray.push(" Boop!, ")
       } else if (i === 3) {
-        outputArray.push(" Won't you be my neighbor?")
+        outputArray.push(" Won't you be my neighbor?, ")
       } else if (i > 3) {
-        outputArray.push(" " + i)
+        outputArray.push(i)
+        if (i != input){
+          outputArray.push(", ")
+        }
       } 
     }
   }
@@ -44,12 +62,16 @@ function beepBoop(input){
 // ui logic
 
 $(document).ready(function(){
-  $("form").submit(function(event){
+  $("#formOne").submit(function(event){
     event.preventDefault();
     let formInput = $("#input").val();
     $("#user-input").hide()
-    $("#results").text(beepBoop(formInput));
+    $("#results").html(beepBoop(formInput));
     $("#results").show();
-    $("form")[0].reset();
+    $("#formOne")[0].reset();
+    $("#formTwo").submit(function(event){
+      $("#results").hide();
+      $("#user-input").show()
+    })
   });
 });
