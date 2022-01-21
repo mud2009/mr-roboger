46526af5dc22ca910ed6a100f6e6c0d9d54b08da
Will Greenberg <greenberg.will@gmail.com>
2022-01-21 15:14:13 -0800

    add second button, hide results section, add spaces to strings, show results using jquery

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 
 // maybe refactor with regular expressions if i have bunch of time left
 // maybe add index number after substitutions to keep track of which strings are which
+// maybe add a more elegant way of adding spaces
+
 
 function beepBoop(input){
   let outputArray = [];
@@ -15,24 +17,24 @@ function beepBoop(input){
         let indexString = i.toString();
         let indexArray = indexString.split("");
         if (indexArray.includes("3")){
-          outputArray.push("Won't you be my neighbor?");
+          outputArray.push(" Won't you be my neighbor?");
         } else if (indexArray.includes("2")){
-          outputArray.push("Boop!");
+          outputArray.push(" Boop!");
         } else if (indexArray.includes("1")){
-          outputArray.push("Beep!");
+          outputArray.push(" Beep!");
         } else {
         let joinedIndex = indexArray.join("");
         let joinedNumber = parseInt(joinedIndex);
         outputArray.push(joinedNumber);
         }
       } else if (i === 1) {
-        outputArray.push("Beep!")
+        outputArray.push(" Beep!")
       } else if (i === 2) {
-        outputArray.push("Boop!")
+        outputArray.push(" Boop!")
       } else if (i === 3) {
-        outputArray.push("Won't you be my neighbor?")
+        outputArray.push(" Won't you be my neighbor?")
       } else if (i > 3) {
-        outputArray.push(i)
+        outputArray.push(" " + i)
       } 
     }
   }
@@ -42,10 +44,12 @@ function beepBoop(input){
 // ui logic
 
 $(document).ready(function(){
-  $("#form").submit(function(event){
+  $("form").submit(function(event){
     event.preventDefault();
     let formInput = $("#input").val();
-    $("#result").html(beepBoop(formInput));
-    $("#form")[0].reset();
+    $("#user-input").hide()
+    $("#results").text(beepBoop(formInput));
+    $("#results").show();
+    $("form")[0].reset();
   });
 });
